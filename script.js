950d2f1e5a27a0e38567381c22b595ad8a200be0
Prateek <prateekbisht701@gmail.com>
2023-11-27 12:51:30 +0530

    add js file to make it dynamic

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function bindData(articles) {
 }
 
 function fillDataInCard ( article, cardClone){
-    
+
     const newsImg = cardClone.querySelector('#news-img') ;
     const newsTitle = cardClone.querySelector('#news-title') ;
     const newsSource = cardClone.querySelector('#news-source') ;
@@ -50,55 +50,3 @@ function fillDataInCard ( article, cardClone){
     })
 }
 
-let currentSelectedNav = null ;
-
-function onNavItemClick (id) {
-    fetchNews(id) ; 
-
-    const navItem = document.getElementById(id) ;
-    currentSelectedNav?.classList.remove('active') ;
-    currentSelectedNav = navItem ;
-    currentSelectedNav.classList.add('active') ;
-}
-
-const searchButton = document.getElementById('search-button') ;
-const searchText = document.getElementById('search-text') ;
-
-searchButton.addEventListener('click', () => {
-    const query = searchText.value ;
-    if(!query) return ; 
-    fetchNews(query) ;
-    currentSelectedNav?.classList.remove('active')
-})
-
-
-// For responsive Navbar part of the website.
-
-const searchIcon = document.querySelector('.search-icon')
-const closeIcon = document.querySelector('.close-icon')
-const navbar = document.querySelector('.nav')
-
-searchIcon.addEventListener('click', () => {
-    const query = searchText.value ;
-    console.log(query)
-    if(!query) return ; 
-    fetchNews(query) ;
-    currentSelectedNav?.classList.remove('active')
-})
-
-searchIcon.addEventListener('click', () => {
-    // console.log(navbar.classList.contains('actives'))
-    if(navbar.classList.contains('actives')){
-        searchText.value = ''
-      }
-      else {
-        navbar.classList.add('actives');
-        searchText.focus();
-      }
-
-})
-
-closeIcon.addEventListener('click',() => {
-    navbar.classList.remove('actives');
-    searchText.value = '';
-})
